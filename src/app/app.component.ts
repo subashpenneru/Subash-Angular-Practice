595989d2e07f13dc55904c49bd2bdf0e4ea9085b
subashpenneru <subashpenneru@gmail.com>
2020-06-08 23:40:35 +0530

    pipes basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { of, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Practice';
+
+  asyncData: any;
+
+  constructor() {
+    const obs = new Observable(this.asyncSubscriber);
+    obs.subscribe(res => {
+      this.asyncData = res;
+    });
+  }
+
+  servers = [
+    { name: 'server1', date: new Date('2020-01-01'), status: 'online' },
+    { name: 'server2', date: new Date('2020-01-02'), status: 'online' },
+    { name: 'server3', date: new Date('2020-01-03'), status: 'offline' },
+    { name: 'server4', date: new Date('2020-01-04'), status: 'online' },
+  ]
+
+  getServerClass(status) {
+    return {
+      'Online': status === 'online',
+      'Offline': status === 'offline'
+    }
+  }
+
+  onAddServer() {
+    const length = this.servers.length;
+    const newServer = {
+      name: `server${length+1}`,
+      date: new Date(),
+      status: this.servers[length-1].status === 'offline' ? 'online' : 'offline'
+    };
+    this.servers.push(newServer);
+  }
+
+  asyncSubscriber = (observer) => {
+    setTimeout(() => observer.next([1,2,3,4,5,6]), 5000);
+  }
+
 }

ab753acd7d10ae77c6ac24d98a6f48b81c0fb0ee
Subash Penneru <subashpenneru@gmail.com>
2020-10-14 10:03:51 +0530

    eslint modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { of, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +9,7 @@ import { of, Observable } from 'rxjs';
 export class AppComponent {
 
   asyncData: any;
-  filteredStatus = ''
+  filteredStatus = '';
 
   constructor() {
     this.asyncData = new Observable(this.asyncSubscriber);
@@ -20,27 +20,27 @@ export class AppComponent {
     { name: 'server2', date: new Date('2020-01-02'), status: 'online' },
     { name: 'server3', date: new Date('2020-01-03'), status: 'offline' },
     { name: 'server4', date: new Date('2020-01-04'), status: 'online' },
-  ]
+  ];
 
   getServerClass(status) {
     return {
-      'Online': status === 'online',
-      'Offline': status === 'offline'
-    }
+      Online: status === 'online',
+      Offline: status === 'offline'
+    };
   }
 
   onAddServer() {
     const length = this.servers.length;
     const newServer = {
-      name: `server${length+1}`,
+      name: `server${length + 1}`,
       date: new Date(),
-      status: this.servers[length-1].status === 'offline' ? 'online' : 'offline'
+      status: this.servers[length - 1].status === 'offline' ? 'online' : 'offline'
     };
     this.servers.push(newServer);
   }
 
   asyncSubscriber = (observer) => {
-    setTimeout(() => observer.next([1,2,3,4,5,6]), 5000);
+    setTimeout(() => observer.next([1, 2, 3, 4, 5, 6]), 5000);
   }
 
 }
--- src/app/server-filter.pipe.ts
+++ src/app/server-filter.pipe.ts
@@ -6,12 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ServerFilterPipe implements PipeTransform {
 
     transform(value: any, limit: any, prop: any) {
-        if(value.length === 0 || limit.trim() === '') {
+        if (value.length === 0 || limit.trim() === '') {
             return value;
         } else {
             const resArr = [];
             value.forEach(e => {
-                if(e[prop].includes(limit)) {
+                if (e[prop].includes(limit)) {
                     resArr.push(e);
                 }
             });

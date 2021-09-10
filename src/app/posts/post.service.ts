ba602990e43420b27bb4c8e6de3ef92b799a4ed3
Subash Penneru <subashpenneru@gmail.com>
2021-09-10 17:15:38 +0530

    comment added

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,33 +1,37 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { throwError, BehaviorSubject } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from "@angular/common/http";
+import { environment } from "src/environments/environment";
+import { throwError, BehaviorSubject } from "rxjs";
+import { map, catchError } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class PostService {
-
-  public postInfo = new BehaviorSubject<{postId: number, userId: number}>({ 
-    postId: -1, 
-    userId: -1 
+  public postInfo = new BehaviorSubject<{ postId: number; userId: number }>({
+    postId: -1,
+    userId: -1,
   });
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getPosts() {
-    const httpHeaders = new HttpHeaders(this.getHeaderOptions());
-    return this.http.get(environment.apiUrl + 'posts', { headers: httpHeaders })
+    const httpHeaders = new HttpHeaders(this.getHeaderOptions()); // Headers not required in this method.
+    return this.http
+      .get(environment.apiUrl + "posts", { headers: httpHeaders })
       .pipe(
         map((postData: any) => {
-          if(postData.length > 10) {
+          if (postData.length > 10) {
             return postData.slice(0, 10);
           }
           return postData;
         }),
-        catchError(error => this.handleError(error))
-      )
+        catchError((error) => this.handleError(error))
+      );
   }
 
   getSinglePost(id) {
@@ -36,20 +40,20 @@ export class PostService {
 
   getHeaderOptions() {
     return {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    }
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
   }
 
   handleError(error: HttpErrorResponse) {
-    const errorObj = { url: '', message: '' }
-    let url = error.url.split('/');
-    errorObj.url = url[url.length-1];
+    const errorObj = { url: "", message: "" };
+    let url = error.url.split("/");
+    errorObj.url = url[url.length - 1];
 
-    if(error.error.message) {
+    if (error.error.message) {
       errorObj.message = error.error.message;
     } else {
-      errorObj.message = error.message
+      errorObj.message = error.message;
     }
 
     return throwError(errorObj);

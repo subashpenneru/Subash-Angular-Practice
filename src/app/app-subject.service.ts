9e58c7c2bcdf2ca71a2407f5a1535824dc7b512f
Subash Penneru <subashpenneru@gmail.com>
2021-11-14 10:01:43 +0530

    updates

--- src/app/app-subject.service.ts
+++ src/app/app-subject.service.ts
@@ -3,8 +3,8 @@ import { Subject, BehaviorSubject, ReplaySubject, AsyncSubject } from "rxjs";
 
 @Injectable()
 export class AppSubjectService {
-  userSub = new Subject<Object>();
+  userSub = new Subject<{ name: string; age: number }>();
   userBehSub = new BehaviorSubject({ name: "Service Behav", age: 25 });
   userReplaySub = new ReplaySubject(2);
-  userAsyncSub = new AsyncSubject<Object>();
+  userAsyncSub = new AsyncSubject<{ name: string; age: number }>();
 }
